2cc3dfb6d6347ac026eee7f87259b37bfe044e84
Siim Kallas <siimkallas@gmail.com>
2014-02-11 22:32:05 +0200

    World query.

--- include/Box2DSimulation.hpp
+++ include/Box2DSimulation.hpp
@@ -16,12 +16,16 @@ private:
 	struct Particle
 	{
 		glm::vec2 position{ 0.0f, 0.0f };
+		glm::vec2 prevPosition{ 0.0f, 0.0f };
 		glm::vec2 velocity{ 0.0f, 0.0f };
 		bool living = false;
 		size_t neighbourCount = 0;
 		size_t gridX = 0;
 		size_t gridY = 0;
 		size_t index = 0;
+		size_t fixturesToTest = 0;
+		float pressure = 0.0f;
+		float pnear = 0.0f;
 		std::vector<size_t> neighbourIndices;
 	};
 
@@ -35,14 +39,18 @@ private:
 	Array2D<size_t> grid;
 	std::vector<glm::vec2> scaledPositions;
 	std::vector<glm::vec2> scaledVelocities;
-
 	std::vector<glm::vec2> deltas;
+
+	b2AABB worldBounds;
 	
 	const float simulationRadius = 0.6f;
 	const float idealRadius = 50.f;
 	const float idealRadiusSquared = idealRadius * idealRadius;
 	const float multiplier = idealRadius / simulationRadius;
 	const float viscosity = 0.004f;
+	const float cellSize = 0.6f;
+
+	size_t convertToGridIndex(float x) const { return static_cast<size_t>(glm::floor(x / cellSize)); }
 	
 public:
 	Box2DSimulation();
@@ -54,6 +62,7 @@ public:
 	void setDragging(bool drag) override;
 	void setInputPosition(glm::vec2 position) override;
 
+	void prepareCollisions();
 	void applyLiquidConstraints();
 	void prepareSimulation(size_t particleIndex);
 	void findNeighbours(Particle& p);
--- src/Box2DSimulation.cpp
+++ src/Box2DSimulation.cpp
@@ -4,6 +4,18 @@
 // Simulation is a port of this: https://github.com/klutch/Box2DFluid
 // TODO: EVERYTHING
 
+class QueryResult : public b2QueryCallback
+{
+public:
+	std::vector<b2Fixture*> fixtures;
+
+	bool ReportFixture(b2Fixture* fixture) override
+	{
+		fixtures.push_back(fixture);
+		return true;
+	}
+};
+
 Box2DSimulation::Box2DSimulation()
 	: physicsWorld(new b2World(b2Vec2{ 0.0f, 9.8f }))
 {
@@ -34,11 +46,39 @@ void Box2DSimulation::findNeighbours(Particle& p)
 	}
 }
 
+void Box2DSimulation::prepareCollisions()
+{
+	QueryResult queryResult;
+	physicsWorld->QueryAABB(&queryResult, worldBounds);
+
+	for (auto fixture : queryResult.fixtures)
+	{
+		b2AABB aabb;
+		b2Transform transform = fixture->GetBody()->GetTransform();
+		fixture->GetShape()->ComputeAABB(&aabb, transform, 0);
+
+		// Top left
+		size_t Ax = convertToGridIndex(aabb.upperBound.x);
+		size_t Ay = convertToGridIndex(aabb.upperBound.y);
+		// Bottom right
+		size_t Bx = convertToGridIndex(aabb.lowerBound.x) + 1;
+		size_t BY = convertToGridIndex(aabb.lowerBound.y) + 1;
+	}
+}
+
 void Box2DSimulation::prepareSimulation(size_t particleIndex)
 {
 	auto& particle = particles[particleIndex];
 
 	findNeighbours(particle);
+	scaledPositions[particleIndex] = particle.position * multiplier;
+	scaledVelocities[particleIndex] = particle.velocity * multiplier;
+	deltas[particleIndex] = glm::vec2{ 0.0f, 0.0f };
+
+	particle.prevPosition = glm::vec2{ 0.0f, 0.0f };
+	particle.fixturesToTest = 0;
+	particle.pressure = 0.0f;
+	particle.pnear = 0.0f;
 }
 
 void Box2DSimulation::createParticles(size_t horizontalAmount, size_t verticalAmount)
@@ -54,7 +94,6 @@ void Box2DSimulation::createParticles(size_t horizontalAmount, size_t verticalAm
 	deltas = std::vector<glm::vec2>{particleCount};
 	scaledPositions = std::vector<glm::vec2>{particleCount};
 	scaledVelocities = std::vector<glm::vec2>{particleCount};
-
 }
 
 void Box2DSimulation::applyLiquidConstraints()
