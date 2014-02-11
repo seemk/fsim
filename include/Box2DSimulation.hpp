134f9d1bd2dd1717f0680f47fb61e680b5b73c2c
Siim Kallas <siimkallas@gmail.com>
2014-02-11 22:02:26 +0200

    Ported over some methods.

--- include/Box2DSimulation.hpp
+++ include/Box2DSimulation.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "FluidSimulation.hpp"
+#include "Array2D.hpp"
 #include <Box2D/Dynamics/b2World.h>
 #include <vector>
 #include <memory>
@@ -9,6 +10,40 @@ class b2World;
 
 class Box2DSimulation : public FluidSimulation
 {
+
+private:
+
+	struct Particle
+	{
+		glm::vec2 position{ 0.0f, 0.0f };
+		glm::vec2 velocity{ 0.0f, 0.0f };
+		bool living = false;
+		size_t neighbourCount = 0;
+		size_t gridX = 0;
+		size_t gridY = 0;
+		size_t index = 0;
+		std::vector<size_t> neighbourIndices;
+	};
+
+	int velocityIterations = 8;
+	int positionIterations = 8;
+	std::unique_ptr<b2World> physicsWorld;
+
+	std::vector<Particle> particles;
+	std::vector<size_t> aliveParticleIndices;
+	// Grid of indices
+	Array2D<size_t> grid;
+	std::vector<glm::vec2> scaledPositions;
+	std::vector<glm::vec2> scaledVelocities;
+
+	std::vector<glm::vec2> deltas;
+	
+	const float simulationRadius = 0.6f;
+	const float idealRadius = 50.f;
+	const float idealRadiusSquared = idealRadius * idealRadius;
+	const float multiplier = idealRadius / simulationRadius;
+	const float viscosity = 0.004f;
+	
 public:
 	Box2DSimulation();
 	void createParticles(size_t horizontalAmount, size_t verticalAmount) override;
@@ -19,10 +54,8 @@ public:
 	void setDragging(bool drag) override;
 	void setInputPosition(glm::vec2 position) override;
 
-private:
-
-	int velocityIterations = 8;
-	int positionIterations = 8;
-	std::unique_ptr<b2World> physicsWorld;
+	void applyLiquidConstraints();
+	void prepareSimulation(size_t particleIndex);
+	void findNeighbours(Particle& p);
 
 };
--- src/Box2DSimulation.cpp
+++ src/Box2DSimulation.cpp
@@ -1,20 +1,81 @@
 #include "Box2DSimulation.hpp"
 #include <Box2D/Box2D.h>
 
+// Simulation is a port of this: https://github.com/klutch/Box2DFluid
+// TODO: EVERYTHING
+
 Box2DSimulation::Box2DSimulation()
 	: physicsWorld(new b2World(b2Vec2{ 0.0f, 9.8f }))
 {
 	physicsWorld->SetAllowSleeping(true);
 }
 
+void Box2DSimulation::findNeighbours(Particle& p)
+{
+	for (int nx = -1; nx < 2; nx++)
+	{
+		for (int ny = -1; ny < 2; ny++)
+		{
+			int x = p.gridX + nx;
+			int y = p.gridY + ny;
+			if (x < 0 || y < 0) continue;
+			size_t gx = static_cast<size_t>(x);
+			size_t gy = static_cast<size_t>(y);
+			// Check if they are in bounds
+			if (gx < grid.cols() && gy < grid.rows())
+			{
+				size_t neighbourIndex = grid(gx, gy);
+				if (neighbourIndex != p.index)
+				{
+					p.neighbourIndices.push_back(neighbourIndex);
+				}
+			}
+		}
+	}
+}
+
+void Box2DSimulation::prepareSimulation(size_t particleIndex)
+{
+	auto& particle = particles[particleIndex];
+
+	findNeighbours(particle);
+}
+
 void Box2DSimulation::createParticles(size_t horizontalAmount, size_t verticalAmount)
 {
+	const size_t particleCount = horizontalAmount * verticalAmount;
+	particles = std::vector<Particle>{particleCount};
+	for (size_t i = 0; i < particles.size(); i++)
+	{
+		particles[i].index = i;
+	}
+	grid = Array2D<size_t>{horizontalAmount, verticalAmount, 0};
+	aliveParticleIndices = std::vector<size_t>{particleCount};
+	deltas = std::vector<glm::vec2>{particleCount};
+	scaledPositions = std::vector<glm::vec2>{particleCount};
+	scaledVelocities = std::vector<glm::vec2>{particleCount};
 
+}
+
+void Box2DSimulation::applyLiquidConstraints()
+{
+	for (auto index : aliveParticleIndices)
+	{
+		const auto& particle = particles[index];
+
+		scaledPositions[index] = particle.position * multiplier;
+		scaledVelocities[index] = particle.velocity * multiplier;
+
+		deltas[index] = glm::vec2{ 0.0f, 0.0f };
+	}
 }
 
 void Box2DSimulation::update(float dt)
 {
-	physicsWorld->Step(dt, velocityIterations, positionIterations);
+	for (auto activeIndex : aliveParticleIndices)
+	{
+		prepareSimulation(activeIndex);
+	}
 }
 
 void Box2DSimulation::addParticle(glm::vec2 location)
